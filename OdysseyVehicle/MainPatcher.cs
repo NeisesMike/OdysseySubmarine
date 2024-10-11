c23c37a84725ddbf54adb0d28472fd3b0f1b5428
Michael Neises <neisesmichael@gmail.com>
2024-10-11 04:49:11 -0500

    Update plugininfo

--- OdysseyVehicle/MainPatcher.cs
+++ OdysseyVehicle/MainPatcher.cs
@@ -37,7 +37,7 @@ namespace OdysseyVehicle
         }
     }
 
-    [BepInPlugin("com.mikjaw.subnautica.odyssey.mod", "Odyssey", "1.4.6")]
+    [BepInPlugin("com.mikjaw.subnautica.odyssey.mod", "Odyssey", "1.5.0")]
     [BepInDependency(VehicleFramework.PluginInfo.PLUGIN_GUID, VehicleFramework.PluginInfo.PLUGIN_VERSION)]
     [BepInDependency("com.snmodding.nautilus")]
     public class MainPatcher : BaseUnityPlugin
