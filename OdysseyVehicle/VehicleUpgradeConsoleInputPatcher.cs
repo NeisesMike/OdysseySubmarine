447a2f0f8c1eea8b437dd456a8f09630b961c99b
Michael Neises <neisesmichael@gmail.com>
2024-12-13 14:49:57 -0600

    Update upgrade hatch logic

--- OdysseyVehicle/VehicleUpgradeConsoleInputPatcher.cs
+++ OdysseyVehicle/VehicleUpgradeConsoleInputPatcher.cs
@@ -19,7 +19,7 @@ namespace OdysseyVehicle
         static Coroutine closeDoorCor = null;
         public static IEnumerator closeDoorSoon(Animator mainAnimator)
         {
-            while(timeUntilClose > 0)
+            while (timeUntilClose > 0)
             {
                 timeUntilClose -= Time.deltaTime;
                 yield return null;
@@ -37,10 +37,32 @@ namespace OdysseyVehicle
             {
                 __instance.GetComponentInParent<Odyssey>().mainAnimator.SetBool("OD_module_door", true);
                 timeUntilClose = openDuration;
-                if(closeDoorCor == null)
+                if (closeDoorCor == null)
                 {
                     closeDoorCor = UWE.CoroutineHost.StartCoroutine(closeDoorSoon(__instance.GetComponentInParent<Odyssey>().mainAnimator));
                 }
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(VehicleUpgradeConsoleInput.OpenPDA))]
+        public static void VehicleUpgradeConsoleInputOpenPDAPostfix(VehicleUpgradeConsoleInput __instance, Sequence ___sequence)
+        {
+            // control opening the modules hatch
+            if (__instance.GetComponentInParent<Odyssey>() != null)
+            {
+                UWE.CoroutineHost.StopCoroutine(closeDoorCor);
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(VehicleUpgradeConsoleInput.OnClosePDA))]
+        public static void VehicleUpgradeConsoleInputOnClosePDAPostfix(VehicleUpgradeConsoleInput __instance, Sequence ___sequence)
+        {
+            // control opening the modules hatch
+            if (__instance.GetComponentInParent<Odyssey>() != null)
+            {
+                closeDoorCor = UWE.CoroutineHost.StartCoroutine(closeDoorSoon(__instance.GetComponentInParent<Odyssey>().mainAnimator));
             }
         }
     }
